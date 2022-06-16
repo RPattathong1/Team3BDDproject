0bdf2c54595d464a9e1aa04baca78aa522ab1bdb
NikolayGarmaev <nikolaygarmaev91@gmail.com>
2022-06-16 23:51:25 +0300

    removed unused imports and @FindBy

--- src/Test/java/pages/LoginPage.java
+++ src/Test/java/pages/LoginPage.java
@@ -1,4 +1,12 @@
 package pages;
 
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.WebDriverManager;
+
 public class LoginPage {
+    public LoginPage() {
+        PageFactory.initElements(WebDriverManager.getDriver(), this);
+    }
+
 }
